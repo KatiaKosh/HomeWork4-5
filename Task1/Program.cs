4f26b02baa97b1e4d70e19515f4203bc8f35cdcc
katia <katia00712@mail.ru>
2022-10-04 15:36:55 +0500

    New file

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+int step = a;
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("Степень числа равна: " + step);
